8d4df88a01cfcf241206cb359792cf8761997557
mhairimccrindle@gmail.com <mhairimccrindle@gmail.com>
2019-09-23 16:31:43 +0100

    waterbottle completed

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -2,5 +2,23 @@ public class WaterBottle {
 
     private double volume;
 
-    
+    public WaterBottle(double volume){
+        this.volume = volume;
+    }
+
+    public double getVolume(){
+        return volume;
+    }
+
+    public double takeDrink(){
+        return volume -= 10;
+    }
+
+    public double emptyBottle(){
+        return volume = 0;
+    }
+
+    public double fillBottle(){
+        return volume = 100;
+    }
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+
+    WaterBottle waterBottle;
+
+    @Before
+    public void before(){
+        waterBottle = new WaterBottle(100);
+    }
+
+    @Test
+    public void hasVolume(){
+        assertEquals(100, waterBottle.getVolume(), 0.01);
+    }
+
+    @Test
+    public void canTakeDrinkFromBottle(){
+        assertEquals(90, waterBottle.takeDrink(), 0.01);
+    }
+
+    @Test
+    public void canEmptyTheBottle(){
+        assertEquals(0, waterBottle.emptyBottle(), 0.01);
+    }
+
+    @Test
+    public void canRefillBottle(){
+        assertEquals(100, waterBottle.fillBottle(), 0.01);
+    }
 }
